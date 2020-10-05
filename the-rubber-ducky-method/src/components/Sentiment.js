0ec076f2414e9fb36d3e38e150a763d1fba5c662
Frederick Ernest <frederick.ernest@gmail.com>
2020-10-05 13:43:53 -0700

    update header to reflect project.

---
+++ the-rubber-ducky-method/src/components/Sentiment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sentiment from 'sentiment';
+
+const sentiment = new Sentiment();
+
+class NaturalLanguage extends React.Component {
+  
+
+}
+
+export default NaturalLanguage
